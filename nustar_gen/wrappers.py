9d1959af6c0f5b3611815b0fc192b7dbd97fe5da
BrianG <bwgref@gmail.com>
2019-11-18 17:40:48 -0800

    Added wrapper for DET1 lightcurves.
    WIP

--- nustar_gen/wrappers.py
+++ nustar_gen/wrappers.py
@@ -342,6 +342,153 @@ def make_image(infile, elow = 3, ehigh = 20, clobber=True, outpath=False):
     return outfile
 
 
+
+
+
+def _make_xselect_commands_det1_evts(infile, outfile, regfile):
+    '''
+    Helper script to generate the xselect commands to make an image in a given NuSTAR range
+    '''
+    
+    import glob
+    for oldfile in glob.glob("session1*"):
+        os.system(f"rm {oldfile}")
+    
+    xsel=open("xsel.xco","w")
+    xsel.write("session1\n")
+    xsel.write("read events \n")
+    evdir=os.path.dirname(infile)
+    xsel.write(f'{evdir} \n ' )
+    evfile = os.path.basename(infile)
+    xsel.write(f'{evfile} \n ')
+    xsel.write('yes \n')
+    xsel.write('set xyname\n')
+    xsel.write('DET1X\n')
+    xsel.write('DET1Y\n')
+    xsel.write(f'filter region {regfile} \n')
+    xsel.write("extract events\n")
+    xsel.write("save events\n")
+    xsel.write("%s \n" % outfile)
+    xsel.write('n \n')
+    xsel.write('exit\n')           
+    xsel.write('n \n')
+    xsel.close()
+    return 'xsel.xco'
+
+
+
+
+def _make_xselect_commands(infile, outfile, elow, ehigh):
+    '''
+    Helper script to generate the xselect commands to make an image in a given NuSTAR range
+    '''
+    
+    xsel=open("xsel.xco","w")
+    xsel.write("session1\n")
+    xsel.write("read events \n")
+    xsel.write('./ \n ' )
+    xsel.write('%s \n ' % infile)
+    xsel.write('yes \n')
+    pi_low = energy_to_chan(elow)
+    pi_high = energy_to_chan(ehigh)
+    xsel.write('filter pha_cutoff {} {} \n'.format(pi_low, pi_high))
+    xsel.write('set xybinsize 1\n')
+    xsel.write("extract image\n")
+    xsel.write("save image\n")
+    xsel.write("%s \n" % outfile)
+    xsel.write('exit\n')           
+    xsel.write('n \n')
+    xsel.close()
+    return 'xsel.xco'
+
+
+def _check_environment():
+    try:
+        if ("CALDB" in os.environ) & ("HEADAS" in os.environ):
+            pass
+    except IOerror:
+        raise IOError("Environment variables $CALDB and $HEADAS not set")
+
+
+###
+###
+###
+###
+###
+###
+
+# From here down are DET1 methods
+
+###
+###
+###
+###
+###
+###
+
+
+def make_det1_image(infile, elow = 3, ehigh = 20, clobber=True, outpath=False):
+    '''
+    Spawn an xselect instance that produces a DET1 image in the energy range.
+    
+    Parameters
+    ----------
+    infile: str
+        Full path tot eh file that you want to process
+    elow: float
+        Low-energy band for the image
+    ehigh: float
+        High-energy band for the image
+        
+    Other Parameters
+    ----------------
+    
+    clobber: boolean, optional, default=True
+        Overwrite existing files?
+
+    outpath: str, optional, default=os.path.dirname(infile)
+        Set the destination for output. Defaults to same location as infile.
+   
+    Return
+    -------
+    outfile: str
+        The full path to the output image.
+    
+    '''
+    # Make sure environment is set up properly
+    _check_environment()
+
+    # Check if input file exists:
+    try:
+        with open(infile) as f:
+            pass
+    except IOError:
+        raise IOError("make_image: File does not exist %s" % (infile))
+
+    if not outpath:
+        outdir=os.path.dirname(infile)
+    else:
+        outdir=outpath
+    
+    # Trime the filename:
+    sname=os.path.basename(infile)
+    if sname.endswith('.gz'):
+        sname = os.path.splitext(sname)[0]
+    sname = os.path.splitext(sname)[0]
+    
+    # Generate outfile name
+    outfile = outdir + '/'+sname+f'_{elow}to{ehigh}keV_det1.fits'
+       
+    if (os.path.exists(outfile)) & (~clobber):
+        warnings.warn('make_image: %s exists, use clobber=True to regenerate' % (outfile))
+    else:
+        os.system("rm "+outfile)
+    xsel_file = _make_xselect_commands_det1(infile, outfile, elow, ehigh)
+    os.system("xselect @"+xsel_file)
+    os.system("rm -r -f "+xsel_file)
+    
+    return outfile
+
 def extract_det1_events(infile, regfile, clobber=True, outpath=False):
     '''
     Spawn an xselect instance that produces a new event file screened using a det1 region
@@ -419,101 +566,104 @@ def extract_det1_events(infile, regfile, clobber=True, outpath=False):
     
     return outfile
 
-
-def _make_xselect_commands_det1_evts(infile, outfile, regfile):
+def make_det1_lightcurve(infile, mod,
+    barycorr=False, time_bin=100*u.s, mode='01',
+    outpath='None', elow=3, ehigh=20):
     '''
-    Helper script to generate the xselect commands to make an image in a given NuSTAR range
-    '''
-    
-    import glob
-    for oldfile in glob.glob("session1*"):
-        os.system(f"rm {oldfile}")
-    
-    xsel=open("xsel.xco","w")
-    xsel.write("session1\n")
-    xsel.write("read events \n")
-    evdir=os.path.dirname(infile)
-    xsel.write(f'{evdir} \n ' )
-    evfile = os.path.basename(infile)
-    xsel.write(f'{evfile} \n ')
-    xsel.write('yes \n')
-    xsel.write('set xyname\n')
-    xsel.write('DET1X\n')
-    xsel.write('DET1Y\n')
-    xsel.write(f'filter region {regfile} \n')
-    xsel.write("extract events\n")
-    xsel.write("save events\n")
-    xsel.write("%s \n" % outfile)
-    xsel.write('n \n')
-    xsel.write('exit\n')           
-    xsel.write('n \n')
-    xsel.close()
-    return 'xsel.xco'
-
-
-
-
-def make_det1_image(infile, elow = 3, ehigh = 20, clobber=True, outpath=False):
-    '''
-    Spawn an xselect instance that produces a DET1 image in the energy range.
+    Generate a script to run nuproducts to make a lightcurve using the whole
+    FoV and turning off all vignetting and PSF effects. Assumes that infile 
+    has already been filtered using extract_det1_events().
     
     Parameters
     ----------
+    
     infile: str
-        Full path tot eh file that you want to process
-    elow: float
-        Low-energy band for the image
-    ehigh: float
-        High-energy band for the image
+        Full path to the input event file.
+    
+    mod: str
+        'A' or 'B'
         
-    Other Parameters
-    ----------------
     
-    clobber: boolean, optional, default=True
-        Overwrite existing files?
+    
+    Optional Parameters
+    -------------------
+    
+    bgd_reg: str
+        If not 'None', then must be the full path to the background region file
 
-    outpath: str, optional, default=os.path.dirname(infile)
-        Set the destination for output. Defaults to same location as infile.
-   
-    Return
-    -------
-    outfile: str
-        The full path to the output image.
+    barycorr: bool 
+        Default is 'False'. If 'True', then queries the infile for the OBJ J2000
+        coordinates and uses these for the barycenter correction.
+        
+    elow: float
+        Low-eneryg bound. Default is 3 keV.
     
+    ehigh: float
+        High-energy bound. Default is 20 keV.
+    
+    outpath: str
+        Optional. Default is to put the lightcurves in the same location as infile
+        
+    mode: str
+        Optional. Used primarily if you're doing mode06 analysis and need to specify
+        output names that are more complicated.
+
     '''
+
+    from astropy.io.fits import getheader
+
     # Make sure environment is set up properly
     _check_environment()
 
-    # Check if input file exists:
-    try:
-        with open(infile) as f:
-            pass
-    except IOError:
-        raise IOError("make_image: File does not exist %s" % (infile))
+    # Check to see that all files exist:
+    assert os.path.isfile(infile), 'make_lightcurve: infile does not exist!'
 
-    if not outpath:
-        outdir=os.path.dirname(infile)
+
+    
+    evdir = os.path.dirname(infile)
+    
+    seqid = os.path.basename(os.path.dirname(evdir))
+    
+    if outpath is 'None':
+        outdir = evdir
     else:
-        outdir=outpath
+        outdir = outpath
     
-    # Trime the filename:
-    sname=os.path.basename(infile)
-    if sname.endswith('.gz'):
-        sname = os.path.splitext(sname)[0]
-    sname = os.path.splitext(sname)[0]
     
-    # Generate outfile name
-    outfile = outdir + '/'+sname+f'_{elow}to{ehigh}keV_det1.fits'
-       
-    if (os.path.exists(outfile)) & (~clobber):
-        warnings.warn('make_image: %s exists, use clobber=True to regenerate' % (outfile))
-    else:
-        os.system("rm "+outfile)
-    xsel_file = _make_xselect_commands_det1(infile, outfile, elow, ehigh)
-    os.system("xselect @"+xsel_file)
-    os.system("rm -r -f "+xsel_file)
+    hdr = getheader(infile)
+    ra = hdr['RA_OBJ']
+    dec = hdr['DEC_OBJ']
+
+    time_bin = int((time_bin.to(u.s)).value)
+    stemout = f'nu{seqid}{mod}{mode}_full_FoV_{elow}to{ehigh}_{time_bin}s'
+    lc_script = f'./runlc_{stemout}.sh'    
     
-    return outfile
+    
+    pi_low = energy_to_chan(elow)
+    pi_high = energy_to_chan(ehigh)
+    
+    with open(lc_script, 'w') as f:
+        f.write('nuproducts phafile=NONE bkgphafile=NONE imagefile=NONE ')
+        f.write(f'infile={infile} ')
+        f.write('runmkarf=no runmkrmf=no ')
+        f.write(f'indir={evdir} outdir={outdir} instrument=FPM{mod} ')
+        f.write(f'steminputs=nu{seqid} stemout={stemout} ')
+        f.write(f'bkgextract=no ')
+        f.write(f'binsize={time_bin} ')
+        f.write(f'srcra={ra} srcdec={dec} srcregionfile=DEFAULT srcradius=299 ')
+        
+        # Turn off all of the time-dependent corrections for the pointing here
+        f.write(f'lcpsfflag=no lcexpoflag=no lcvignflag=no ')
+        
+        if barycorr:
+            attorb=evdir+f'/nu{seqid}{mod}.attorb'
+            f.write(f'barycorr=yes srcra_barycorr={ra} srcdec_barycorr={dec} ')
+            f.write(f'orbitfile={attorb} ')
+            
+        f.write('clobber=yes')
+        
+    os.chmod(lc_script, stat.S_IRWXG+stat.S_IRWXU)
+    return lc_script
 
 
 def _make_xselect_commands_det1(infile, outfile, elow, ehigh):
@@ -547,35 +697,3 @@ def _make_xselect_commands_det1(infile, outfile, elow, ehigh):
     xsel.write('n \n')
     xsel.close()
     return 'xsel.xco'
-
-def _make_xselect_commands(infile, outfile, elow, ehigh):
-    '''
-    Helper script to generate the xselect commands to make an image in a given NuSTAR range
-    '''
-    
-    xsel=open("xsel.xco","w")
-    xsel.write("session1\n")
-    xsel.write("read events \n")
-    xsel.write('./ \n ' )
-    xsel.write('%s \n ' % infile)
-    xsel.write('yes \n')
-    pi_low = energy_to_chan(elow)
-    pi_high = energy_to_chan(ehigh)
-    xsel.write('filter pha_cutoff {} {} \n'.format(pi_low, pi_high))
-    xsel.write('set xybinsize 1\n')
-    xsel.write("extract image\n")
-    xsel.write("save image\n")
-    xsel.write("%s \n" % outfile)
-    xsel.write('exit\n')           
-    xsel.write('n \n')
-    xsel.close()
-    return 'xsel.xco'
-
-
-def _check_environment():
-    try:
-        if ("CALDB" in os.environ) & ("HEADAS" in os.environ):
-            pass
-    except IOerror:
-        raise IOError("Environment variables $CALDB and $HEADAS not set")
-
