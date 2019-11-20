64edf4497689ec13051338a7f733361de7241531
BrianG <bwgref@gmail.com>
2019-11-20 13:12:14 -0800

    Added straylight ARF generator

--- nustar_gen/utils.py
+++ nustar_gen/utils.py
@@ -113,7 +113,7 @@ def straylight_area(det1im, regfile, evf):
     from astropy import units as u
     
 
-        # Check to see that all files exist:
+    # Check to see that all files exist:
     assert os.path.isfile(det1im), \
         f'straylight_area: DET1 exposure map not found: {det1im}'
     assert os.path.isfile(regfile), \
@@ -146,3 +146,120 @@ def straylight_area(det1im, regfile, evf):
     return area
     
 
+
+def make_straylight_arf(det1im, regfile, filt_file, mod, outpath=None):
+    '''
+    Produces an ARF for a given observation. Currently uses counts-weighting to
+        determine the contribution of the detabs parameters for each detector.
+        
+    Parameters
+    ----------
+    det1im : str
+        Full path to the DET1 exposure image file.
+        
+    regfil : str
+        Full path to the region file, assumed to be a ds9 region file in "IMAGE"
+        coordinates
+    
+    filt_file : str
+        Full path to the DET1 screened event file (i.e. output of
+        nustar_gen.wrappers.extract_det1_events() ).
+
+    mod : str
+        'A' or 'B'
+
+    Other parameters:
+    -----------------
+    
+    outpath : str
+        Output directory. If not specified, places this in the same location as evf
+
+    Returns
+    -------
+    
+    Full path to the new arf file
+    
+    '''   
+    
+    from astropy.io import fits
+    import glob
+    from nustar_gen import io
+    
+        # Check to see that all files exist:
+    assert os.path.isfile(det1im), \
+        f'straylight_area: DET1 exposure map not found: {det1im}'
+    assert os.path.isfile(regfile), \
+        f'straylight_area: Region file not found: {regfile}'
+    assert os.path.isfile(filt_file), \
+        f'straylight_area: Event file not found: {evf}'
+
+    if outpath is None:
+        outdir = os.path.dirname(evf)
+    else:
+        outdir=outpath
+    
+    out_arf = outdir+'/'+os.path.splitext(os.path.basename(filt_file))[0]+'.arf'
+
+    # Get the effective are target:
+    area = straylight_area(det1im, regfile, filt_file)
+    
+    # Use counts relative scaling to blend the ARFs:
+    ev_hdu = fits.open(filt_file)
+    evts = ev_hdu[1].data
+    dethist, detedges = np.histogram(evts['DET_ID'], range=[0, 3],bins = 4)
+    totcts = dethist.sum()
+    scale = [ float(x) / totcts for x in dethist]
+    ev_hdu.close()
+
+    # Load the base ARF file:
+    arf_file = io.find_arf_template()
+    arf_hdu = fits.open(arf_file)
+    arf = arf_hdu[1].data
+    arf_header = arf_hdu[1].header
+    # Trim header
+    arf_hdu[1].header = arf_header[0:26]
+
+    # Mock up an ARF that's flat with just the detector area
+    arf['SPECRESP'] = [area.value for x in arf['SPECRESP']]
+
+
+    caldb = os.environ['CALDB']
+    detabs_files = glob.glob(caldb+f'/**/nu{mod}detabs*', recursive=True)
+    use = len(detabs_files)-1
+
+    detabs_hdu = fits.open(detabs_files[use])
+
+    for detind, isc in enumerate(scale):
+        detabs = detabs_hdu[detind+1].data
+        if detind == 0:
+            arfabs = detabs['DETABS'] * isc
+        else:
+            arfabs += detabs['DETABS'] * isc
+    arf['SPECRESP'] *= arfabs
+    detabs_hdu.close()
+
+    be_file = io.find_be_arf()
+    be_arf = fits.getdata(be_file, 1)
+
+    arf['SPECRESP'] *= be_arf['SPECRESP']
+
+    arf_hdu.writeto(out_arf, overwrite=True)
+    arf_hdu.close()
+    
+    return out_arf
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
