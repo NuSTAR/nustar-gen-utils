4fd4e0fe4311053ffb855d959cbeef7a74fd6c9d
BrianG <bwgref@gmail.com>
2020-01-09 14:46:15 -0800

    Slight updates to time processing
    Addresses #16 
    Also added some docstring checks

--- nustar_gen/io.py
+++ nustar_gen/io.py
@@ -3,13 +3,51 @@ import warnings
 
 
 def find_be_arf():
+    '''
+    Find and return the ARF for the Be window (for straylight analysis only)
+    
+    Parameters
+    ----------
+    None
+    
+    Returns
+    -------
+    Full path to the Be window file, which should be included when this repo is
+    checked out
+    
+    Examples
+    --------
+    >>> be_file = find_be_arf()
+    >>> print(os.path.isfile(be_file))
+    True
+    
+    '''
     curdir = os.path.dirname(__file__)
     tempdir = os.path.join(curdir, 'templates')
     be_arf_file = os.path.join(tempdir, 'be.arf')
     return be_arf_file
     
 def find_arf_template():
+    '''
+    Find and return the template ARF file.
+    
+    Parameters
+    ----------
+    None
+    
+    Returns
+    -------
+    Full path to the Be window file, which should be included when this repo is
+    checked out
+    
+    Examples
+    --------
+    >>> arf_file = find_arf_template()
+    >>> print(os.path.isfile(arf_file))
+    True
+    
+    '''
     curdir = os.path.dirname(__file__)
     tempdir = os.path.join(curdir, 'templates')
-    be_arf_file = os.path.join(tempdir, 'nu40101010002_srcA_sr.arf')
-    return be_arf_file
+    arf_file = os.path.join(tempdir, 'nu40101010002_srcA_sr.arf')
+    return arf_file
