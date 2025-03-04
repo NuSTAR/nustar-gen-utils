782b8042c749df8d1d15bc55626a766771e42de0
Brian Grefenstette <bwgref@srl.caltech.edu>
2025-03-04 11:34:24 -0800

    Removed astropy.tests
    Removes astropy.tests. Note that we don't even pretend do to any runtime testing now or CI on this repo.

--- nustar_gen/_astropy_init.py
+++ nustar_gen/_astropy_init.py
@@ -8,6 +8,3 @@ try:
 except ImportError:
     __version__ = ''
 
-# Create the test function for self test
-from astropy.tests.runner import TestRunner
-test = TestRunner.make_test_runner_in(os.path.dirname(__file__))
